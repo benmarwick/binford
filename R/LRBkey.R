738be29ee0ccdf44cb4316ec920123f8dd12b42e
Ben Marwick <benmarwick@gmail.com>
2016-08-01 22:50:08 +1000

    minor update to man

--- R/LRBkey.R
+++ R/LRBkey.R
@@ -1,6 +1,6 @@
 #' Key to Binford's data
 #'
-#' Data frame describing the variables in the LRB data frame. Part of the datasets used to calculate Binford's environmental and hunter-gatherer frames of reference variables used in:
+#' Data frame describing the variables in the LRB data frame. Part of the datasets used to calculate Binford's environmental and hunter-gatherer frames of reference variables used in Binford (2001).
 #'
 #' Binford, Lewis R. 2001. Constructing Frames of Reference: An Analytical Method for Archaeological Theory Building Using Ethnographic and Environmental Data Sets University of California Press, Berkeley
 #'
